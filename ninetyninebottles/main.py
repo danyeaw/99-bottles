becb16c4e19bf79f6530595a631a777e735f6ca2
Dan Yeaw <dan@yeaw.me>
2023-01-05 13:37:53 -0500

    Remove function and shorten print statements

--- ninetyninebottles/main.py
+++ ninetyninebottles/main.py
@@ -1,19 +1,18 @@
 from ninetyninebottles.i18n import gettext
 
 
-def print_bottles_stanza(bottle_num: int) -> None:
-    """Prints the stanza for the given number of bottles."""
-    print(gettext("{0} bottles of beer on the wall, {0} bottles of beer.").format(bottle_num))
-    print(gettext("Take one down, pass it around, {} bottles of beer on the wall.").format(bottle_num - 1))
-    if bottle_num == 2:
-        print(gettext("1 bottle of beer on the wall, 1 bottle of beer."))
-        print(gettext("Take one down, pass it around, no more bottles of beer on the wall."))
-
-
 def main() -> None:
     """Prints the lyrics to 99 Bottles of Beer."""
-    for bottle_num in range(99, 1, -1):
-        print_bottles_stanza(bottle_num)
+    for bottles in range(99, 1, -1):
+        print(gettext("{} bottles of beer on the wall,").format(bottles))
+        print(gettext("{} bottles of beer.").format(bottles))
+        print(gettext("Take one down, pass it around,").format(bottles - 1))
+        print(gettext("{} bottles of beer on the wall.").format(bottles - 1))
+
+    print(gettext("1 bottle of beer on the wall,"))
+    print(gettext("1 bottle of beer."))
+    print(gettext("Take one down, pass it around,"))
+    print(gettext("no more bottles of beer on the wall."))
 
 
 if __name__ == "__main__":
